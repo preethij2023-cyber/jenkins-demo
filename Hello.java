77d1a636a5d88c3a24d7ee8786458d0fcf4a99af
preethij2023-cyber <preethi.j2023@vitstudent.ac.in>
2026-02-09 12:09:31 +0530

    Fix Java main class

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
-public class Hello {
+class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins Java");
+        System.out.println("Hello from Jenkins Java Build");
     }
 }
